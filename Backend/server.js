6cc52b325defe80f51abdc552d872c273c80a9ff
kartikay-28 <kartikayrattan1@gmail.com>
2025-02-07 10:56:55 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+    res.send("Welcome to my Express server!");
+});
+
 app.get("/ping", (req, res) => {
     res.send("Pong!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Hi, my name is Kartikay Rattan. Server is running on http://localhost:${PORT}`);
 });
